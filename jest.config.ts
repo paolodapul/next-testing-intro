f3517458764babbeb277203e7b4b790a6d5140b4
Paolo Dapul <paolodapultech@gmail.com>
2024-07-17 14:38:07 +0800

    working code with Jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default createJestConfig(config);
